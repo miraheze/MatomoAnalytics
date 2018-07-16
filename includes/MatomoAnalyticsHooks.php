f64c51b84f1d5fd8f8be6a94d702fb4b8de44bd0
John Lewis <johnflewis93@gmail.com>
2018-07-16 22:09:49 +0100

    add matomo JS script

---
+++ includes/MatomoAnalyticsHooks.php
@@ -0,0 +1,57 @@
+<?php
+
+class MatomoAnalyticsHooks {
+	/**
+	* Function to add Matomo JS to all MediaWiki pages
+	*
+	* Adds exclusion for users with 'noanalytics' userright
+	*
+	* @param Skin $skin Skin object
+	* @param string &$text Output text.
+	* @return bool
+	*/
+	public function matomoScript( $skin, &$text = '' ) {
+		global $wgMatomoAnalyticsServerURL, $wgMatomoAnalytisSiteID, $wgUser, $wgDBname;
+		if ( !$wgMatomoAnalyticsSiteID ) {
+			$wgMatomoAnalyticsSiteID = 1;
+		}
+
+		if ( $wgUser->isAllowed( 'noanalytics' ) {
+			$text .= '<!-- MatomoAnalytics: User right noanalytics is assigned. -->';
+		} else {
+			$id = strval( $wgMatomoAnalyticsSiteID );
+			$serverurl = Xml::encodeJsVar( $wgMatomoAnalyticsServerURL );
+			$title = $skin->getRelevantTitle();
+			$jstitle = Xml::encodeJsVar( $title->getPrefixedText() );
+			$dbname = Xml::encodeJsVar( $wgDBname );
+			$urltitle = $title->getPrefixedURL();
+			$userType = $wgUser->isLoggedIn() ? "User" : "Anonymous";
+			$text .= <<<SCRIPT
+				<!-- Piwik -->
+				<script type="text/javascript">
+				var _paq = _paq || [];
+				_paq.push(["trackPageView"]);
+				_paq.push(["enableLinkTracking"]);
+				(function() {
+					var u = "{$serverurl}/";
+					_paq.push(["setTrackerUrl", u + "piwik.php"]);
+					_paq.push(['setDocumentTitle', {$dbname} + " - " + {$jstitle}]);
+					_paq.push(["setSiteId", "{$id}"]);
+					_paq.push(["setCustomVariable", 1, "userType", "{$userType}", "visit"]);
+					var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
+					g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
+				})();
+				</script>
+				<!-- End Piwik Code -->
+				<!-- Piwik Image Tracker -->
+				<noscript>
+					<img src="{$serverurl}/piwik.php?idsite={$id}&amp;rec=1&amp;action_name={$urltitle}" style="border:0" alt="" />
+				</noscript>
+				<!-- End Piwik -->
+SCRIPT;
+		}
+
+		return true;
+	}
+}
+
