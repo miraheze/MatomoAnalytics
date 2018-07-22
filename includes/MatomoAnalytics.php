6b61be659bff514e1b437df22eca3f54b9fac3b9
John Lewis <johnflewis93@gmail.com>
2018-07-22 23:25:04 +0100

    add basic function for addSite

---
+++ includes/MatomoAnalytics.php
@@ -0,0 +1,21 @@
+<?php
+class MatomoAnalytics {
+	private function __construct( $dbname ) {
+		$this->dbname = $dbname;
+	}
+
+	public static function addSite( $dbname ) {
+		global $wgMatomoAnalyticsServerURL, $wgMatomoAnalyticsUseDB, $wgMatomoAnalyticsDatabase, $wgMatomoAnalyticsTokenAuth;
+
+		$queryapi = $wgMatomoAnalyticsServerURL;
+		$queryapi .= '?module=API&format=json&method=SitesManager.addSite';
+		$queryapi .= "&siteName=$dbname";
+		$queryapi .= "&token_auth=$wgMatomoAnalyticsTokenAuth";
+
+		$sitereply = file_get_contents($queryapi);
+		$sitecontent = unseralize($sitereply);
+
+		return $sitecontent['value'];
+	}
+
+}
