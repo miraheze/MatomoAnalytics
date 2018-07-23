8ce87b03b09c9eb25948aaec6c135973a7b7f62b
John Lewis <johnflewis93@gmail.com>
2018-07-23 01:01:56 +0100

    drop AUTO_INCREMENT from matomo_id

--- sql/matomo.sql
+++ sql/matomo.sql
@@ -1,4 +1,4 @@
 CREATE TABLE /*$wgDBprefix*/matomo (
-  `matomo_id` INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `matomo_id` INT unsigned NOT NULL PRIMARY KEY,
   `matomo_wiki` VARCHR(64) NOT NULL,
 ) /*$wgDBTableOptions*/;
