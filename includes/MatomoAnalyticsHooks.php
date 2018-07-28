176ce32f263f028192f95ac5941ea4a51f272e76
paladox <paladox@users.noreply.github.com>
2018-07-28 17:16:16 +0100

    Update MatomoAnalyticsHooks.php

--- includes/MatomoAnalyticsHooks.php
+++ includes/MatomoAnalyticsHooks.php
@@ -41,6 +41,7 @@ class MatomoAnalyticsHooks {
 			$text .= '<!-- MatomoAnalytics: User right noanalytics is assigned. -->';
 		} else {
 			$id = strval( $wgMatomoAnalyticsID );
+			$defaultId = strval( $wgMatomoAnalyticsSiteID );
 			$serverurl = $wgMatomoAnalyticsServerURL;
 			$title = $skin->getRelevantTitle();
 			$jstitle = Xml::encodeJsVar( $title->getPrefixedText() );
@@ -59,7 +60,7 @@ class MatomoAnalyticsHooks {
 					_paq.push(['setDocumentTitle', {$dbname} + " - " + {$jstitle}]);
 					_paq.push(["setSiteId", "{$id}"]);
 					_paq.push(["setCustomVariable", 1, "userType", "{$userType}", "visit"]);
-					_paq.push(['addTracker', u + "piwik.php", 1]);
+					_paq.push(['addTracker', u + "piwik.php", {$defaultId}]);
 					var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
 					g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
 				})();
