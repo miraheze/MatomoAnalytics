d6b178de2ea84ff8f3d4072c42d334e999c5549d
paladox <paladox@users.noreply.github.com>
2018-07-28 18:04:24 +0100

    Update MatomoAnalyticsHooks.php (#4)
    * Update MatomoAnalyticsHooks.php
    
    * Update extension.json
    
    * Update MatomoAnalyticsHooks.php
    
    * Update extension.json

--- includes/MatomoAnalyticsHooks.php
+++ includes/MatomoAnalyticsHooks.php
@@ -35,13 +35,12 @@ class MatomoAnalyticsHooks {
 	* @return bool
 	*/
 	public function matomoScript( $skin, &$text = '' ) {
-		global $wgMatomoAnalyticsServerURL, $wgUser, $wgDBname, $wgMatomoAnalyticsID, $wgMatomoAnalyticsSiteID;
+		global $wgMatomoAnalyticsServerURL, $wgUser, $wgDBname, $wgMatomoAnalyticsID, $wgMatomoAnalyticsGlobalID;
 
 		if ( $wgUser->isAllowed( 'noanalytics' ) ) {
 			$text .= '<!-- MatomoAnalytics: User right noanalytics is assigned. -->';
 		} else {
 			$id = strval( $wgMatomoAnalyticsID );
-			$defaultId = strval( $wgMatomoAnalyticsSiteID );
 			$serverurl = $wgMatomoAnalyticsServerURL;
 			$title = $skin->getRelevantTitle();
 			$jstitle = Xml::encodeJsVar( $title->getPrefixedText() );
@@ -56,11 +55,14 @@ class MatomoAnalyticsHooks {
 				_paq.push(["enableLinkTracking"]);
 				(function() {
 					var u = "{$serverurl}";
+					var globalId = {$wgMatomoAnalyticsGlobalID};
 					_paq.push(["setTrackerUrl", u + "piwik.php"]);
 					_paq.push(['setDocumentTitle', {$dbname} + " - " + {$jstitle}]);
 					_paq.push(["setSiteId", "{$id}"]);
 					_paq.push(["setCustomVariable", 1, "userType", "{$userType}", "visit"]);
-					_paq.push(['addTracker', u + "piwik.php", {$defaultId}]);
+					if ( globalId ) {
+					    _paq.push(['addTracker', u + "piwik.php", globalId]);
+					}
 					var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
 					g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
 				})();
