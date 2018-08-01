b14a0c21509fce9ea468b0f94f378617ca70631a
John Lewis <johnflewis93@gmail.com>
2018-08-02 00:12:30 +0100

    add functions for getting specific matomo data

--- includes/MatomoAnalytics.php
+++ includes/MatomoAnalytics.php
@@ -73,7 +73,7 @@ class MatomoAnalytics {
 		}
 	}
 
-	private static function getAPIData( $dbname, $module, $period, $jsonlabel, $jsondata ) {
+	private static function getAPIData( $dbname, $module, $period = 'month', $jsonlabel = 'label', $jsondata = 'nb_visits' ) {
 		global $wgMatomoAnalyticsServerURL, $wgMatomoAnalyticsTokenAuth;
 
 		$siteid = MatomoAnalytics::getSiteID( $dbname );
@@ -94,7 +94,84 @@ class MatomoAnalytics {
 		return $arrayout;
 	}
 
+	/*
+	 * Below are a lot of functions that return arrays of data from Matomo APIs.
+	 * Each function has a comment above it that states what it gets.
+	 * This data can be manipulated by anyone, however they want.
+	 */
+
+	// Returns number of visits from a browser (Chrome, Firefox, Edge etc.)
+	public static function getBrowserTypes( $dbname ) {
+		return self::getAPIData( $dbname, 'DevicesDetection.getBrowsers' );
+	}
+
+	// Returns number of visits from a type of device (Desktop, Smartphone etc.)
 	public static function getDeviceTypes( $dbname ) {
-		return self::getAPIData( $dbname, 'DevicesDetection.getType', 'month', 'label', 'nb_visits' );
+		return self::getAPIData( $dbname, 'DevicesDetection.getType' );
+	}
+
+	// Returns number of visits from a type of refereer (Search Engine, Websites etc.)
+	public static function getReferrerType( $dbname ) {
+		return self::getAPIData( $dbname, 'Referrers.getReferrerType' );
+	}
+
+	// Returns list of search terms and number of times they were used.
+	public static function getSearchKeywords( $dbname ) {
+		return self::getAPIData( $dbname, 'Referrers.getKeywords' );
+	}
+
+	// Returns number of visits from a social network.
+	public static function getSocialReferrals( $dbname ) {
+		return self::getAPIData( $dbname, 'Referrers.getSocials' );
+	}
+
+	// Returns number of visits that started from another website.
+	public static function getWebsiteReferrals( $dbname ) {
+		return self::getAPIData( $dbname, 'Referrers.getWebsites' );
+	}
+
+	// Returns number of visits per continent.
+	public static function getUsersContinent( $dbname ) {
+		return self::getAPIData( $dbname, 'UserCountry.getContinent' );
+	}
+
+	// Returns number of visits per country.
+	public static function getUsersCountry( $dbname ) {
+		return self::getAPIData( $dbname, 'UserCountry.getCountry' );
+	}
+
+	// Returns number of visits per day.
+	public static function getVisitsByDay( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitTime.getByDayOfWeek' );
+	}
+
+	// Returns number of visits started per each server hour (24 hours, server timezone).
+	public static function getVisitsPerServerHour( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitTime.getVisitInformationPerServerTime' );
+	}
+
+	// Returns page groupings for visit sessions (1 pages, 2-3 pages, 4-5 pages etc.).
+	public static function getVisitPages( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitorInterest.getNumberOfVisitsPerPage' );
+	}
+
+	// Returns time groupings for visit sessions (0-10s, 11-60s, 1-5m etc.).
+	public static function getVisitDurations( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitorInterest.getNumberOfVisitsPerVisitDuration' );
+	}
+
+	// Returns number of days passed between visit sessions (first visit, 0 days, 1 day, 2 days etc.).
+	public static function getVisitDaysPassed( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitorInterest.getNumberOfVisitsByDaysSinceLast' );
+	}
+
+	// Returns number of visits grouped by total number of visits.
+	public static function getVisitsCount( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitorInterest.getNumberOfVisitsByVisitCount' );
+	}
+
+	// Returns percentage of visit shares by total number of visits.
+	public static function getVisitsCountPercentage( $dbname ) {
+		return self::getAPIData( $dbname, 'VisitorInterest.getNumberOfVisitsByVisitCount', $jsondata = 'nb_visits_percentage' );
 	}
 }
