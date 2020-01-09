f1ecc42df244c8836331b23a4c1f73748407b7b3
paladox <paladox@users.noreply.github.com>
2020-01-09 19:27:07 +0000

    Convert matomo.matomo_wiki to indexes

--- sql/matomo.sql
+++ sql/matomo.sql
@@ -2,3 +2,5 @@ CREATE TABLE /*$wgDBprefix*/matomo (
   `matomo_id` INT unsigned NOT NULL PRIMARY KEY,
   `matomo_wiki` VARCHAR(64) NOT NULL
 ) /*$wgDBTableOptions*/;
+
+CREATE INDEX /*i*/matomo_wiki ON /*_*/matomo (matomo_wiki);
