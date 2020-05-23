457b3ef04e94a2a62841a8021a8c845d6a9140b7
paladox <paladox@users.noreply.github.com>
2020-05-23 19:00:35 +0100

    Add script that adds or removes a wiki from matomo (#25)
    * Add script that adds or removes a wiki from matomo
    
    * Update modifyMatomo.php
    
    * Update modifyMatomo.php
    
    * Update modifyMatomo.php

---
+++ maintenance/modifyMatomo.php
@@ -0,0 +1,28 @@
+<?php
+
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = __DIR__ . '/../../..';
+}
+require_once "$IP/maintenance/Maintenance.php";
+
+class ModifyMatomo extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = "Adds or remove a wiki from matomo.";
+		$this->addOption( 'remove', 'Removes wiki from matomo', false, false );
+	}
+
+	public function execute() {
+		global $wgDBname;
+
+		if ( $this->getOption( 'remove' ) ) {
+			MatomoAnalyticsHooks::wikiDeletion( null, $wgDBname );
+		} else {
+			MatomoAnalyticsHooks::wikiCreation( $wgDBname );
+		}
+	}
+}
+
+$maintClass = 'ModifyMatomo';
+require_once RUN_MAINTENANCE_IF_MAIN;
