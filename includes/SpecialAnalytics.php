01522d8f48f719947198f8eb8f7e5e0c428f6758
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-06-29 12:17:55 -0600

    Update SpecialAnalytics.php

--- includes/SpecialAnalytics.php
+++ includes/SpecialAnalytics.php
@@ -14,21 +14,13 @@ class SpecialAnalytics extends SpecialPage {
 
 		$out = $this->getOutput();
 		$out->addWikiMsg( 'matomoanalytics-header' );
+
 		$out->addModules( [ 'ext.matomoanalytics.oouiform' ] );
+		$out->addModuleStyles( ['ext.matomoanalytics.oouiform.styles'] );
+		$out->addModuleStyles( [ 'oojs-ui-widgets.styles' ] );
 
 		$analyticsViewer = new MatomoAnalyticsViewer();
 		$htmlForm = $analyticsViewer->getForm( $this->getContext() );
-		$sectionTitles = $htmlForm->getFormSections();
-
-		$sectTabs = [];
-		foreach ( $sectionTitles as $key ) {
-			$sectTabs[] = [
-				'name' => $key,
-				'label' => $htmlForm->getLegend( $key )
-			];
-		}
-
-		$out->addJsConfigVars( 'wgMatomoAnalyticsOOUIFormTabs', $sectTabs );
 
 		$htmlForm->show();
 	}
