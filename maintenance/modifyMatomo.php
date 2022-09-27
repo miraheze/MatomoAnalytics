ca437013ea946b998cfbb0f9f4b4d9a1dc1f2560
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-26 19:03:14 -0600

    Don't use Maintenance::$mDescription directly (#85)
    Deprecated since 1.39

--- maintenance/modifyMatomo.php
+++ maintenance/modifyMatomo.php
@@ -9,7 +9,8 @@ require_once "$IP/maintenance/Maintenance.php";
 class ModifyMatomo extends Maintenance {
 	public function __construct() {
 		parent::__construct();
-		$this->mDescription = 'Add or remove a wiki from matomo.';
+
+		$this->addDescription( 'Add or remove a wiki from matomo.' );
 		$this->addOption( 'remove', 'Remove wiki from matomo', false, false );
 	}
 
