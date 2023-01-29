733a58840a94fcd6e975dff693a3227c974310f9
paladox <paladox@users.noreply.github.com>
2023-01-29 16:51:16 +0000

    Don't override $text within MatomoAnalyticsHooks::matomoScript (#93)
    This should prevent duplicates.

--- includes/MatomoAnalyticsHooks.php
+++ includes/MatomoAnalyticsHooks.php
@@ -32,13 +32,11 @@ class MatomoAnalyticsHooks {
 	 * @param string &$text Output text.
 	 * @return bool
 	 */
-	public static function matomoScript( $skin, &$text = '' ) {
+	public static function matomoScript( $skin, &$text ) {
 		$config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig( 'matomoanalytics' );
 
-		static $alreadyDone = false;
-
-		// Check if JS tracking is disabled or if the script has already been added and bow out early
-		if ( $alreadyDone || $config->get( 'MatomoAnalyticsDisableJS' ) ) {
+		// Check if JS tracking is disabled and bow out early
+		if ( $config->get( 'MatomoAnalyticsDisableJS' ) === true ) {
 			return true;
 		}
 
@@ -47,49 +45,48 @@ class MatomoAnalyticsHooks {
 
 		$permissionManager = MediaWikiServices::getInstance()->getPermissionManager();
 		if ( $permissionManager->userHasRight( $user, 'noanalytics' ) ) {
-			$text .= '<!-- MatomoAnalytics: User right noanalytics is assigned. -->';
-		} else {
-			$id = strval( $mAId );
-			$globalId = (string)$config->get( 'MatomoAnalyticsGlobalID' );
-			$globalIdInt = (int)$globalId;
-			$serverurl = $config->get( 'MatomoAnalyticsServerURL' );
-			$title = $skin->getRelevantTitle();
-
-			$jstitle = Xml::encodeJsVar( $title->getPrefixedText() );
-			$dbname = Xml::encodeJsVar( $config->get( 'DBname' ) );
-			$urltitle = $title->getPrefixedURL();
-			$userType = $user->isRegistered() ? 'User' : 'Anonymous';
-			$cookieDisable = (int)$config->get( 'MatomoAnalyticsDisableCookie' );
-			$forceGetRequest = (int)$config->get( 'MatomoAnalyticsForceGetRequest' );
-			$text .= <<<SCRIPT
-				<script>
-				var _paq = window._paq = window._paq || [];
-				if ( {$cookieDisable} ) {
-					_paq.push(['disableCookies']);
-				}
-				if ( {$forceGetRequest} ) {
-					_paq.push(['setRequestMethod', 'GET']);
-				}
-				_paq.push(['trackPageView']);
-				_paq.push(['enableLinkTracking']);
-				(function() {
-					var u = "{$serverurl}";
-					_paq.push(['setTrackerUrl', u+'matomo.php']);
-					_paq.push(['setDocumentTitle', {$dbname} + " - " + {$jstitle}]);
-					_paq.push(['setSiteId', {$id}]);
-					_paq.push(['setCustomVariable', 1, 'userType', "{$userType}", "visit"]);
-					if ( {$globalIdInt} ) {
-					    _paq.push(['addTracker', u + 'matomo.php', {$globalId}]);
-					}
-					var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-					g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-				})();
-				</script>
-				<noscript><p><img src="{$serverurl}matomo.php?idsite={$id}&amp;rec=1&amp;action_name={$urltitle}" style="border:0;" alt="" /></p></noscript>
-			SCRIPT;
+			$text = '<!-- MatomoAnalytics: User right noanalytics is assigned. -->';
+			return true;
 		}
 
-		$alreadyDone = true;
+		$id = strval( $mAId );
+		$globalId = (string)$config->get( 'MatomoAnalyticsGlobalID' );
+		$globalIdInt = (int)$globalId;
+		$serverurl = $config->get( 'MatomoAnalyticsServerURL' );
+		$title = $skin->getRelevantTitle();
+
+		$jstitle = Xml::encodeJsVar( $title->getPrefixedText() );
+		$dbname = Xml::encodeJsVar( $config->get( 'DBname' ) );
+		$urltitle = $title->getPrefixedURL();
+		$userType = $user->isRegistered() ? 'User' : 'Anonymous';
+		$cookieDisable = (int)$config->get( 'MatomoAnalyticsDisableCookie' );
+		$forceGetRequest = (int)$config->get( 'MatomoAnalyticsForceGetRequest' );
+		$text = <<<SCRIPT
+			<script>
+			var _paq = window._paq = window._paq || [];
+			if ( {$cookieDisable} ) {
+				_paq.push(['disableCookies']);
+			}
+			if ( {$forceGetRequest} ) {
+				_paq.push(['setRequestMethod', 'GET']);
+			}
+			_paq.push(['trackPageView']);
+			_paq.push(['enableLinkTracking']);
+			(function() {
+				var u = "{$serverurl}";
+				_paq.push(['setTrackerUrl', u+'matomo.php']);
+				_paq.push(['setDocumentTitle', {$dbname} + " - " + {$jstitle}]);
+				_paq.push(['setSiteId', {$id}]);
+				_paq.push(['setCustomVariable', 1, 'userType', "{$userType}", "visit"]);
+				if ( {$globalIdInt} ) {
+					_paq.push(['addTracker', u + 'matomo.php', {$globalId}]);
+				}
+				var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+				g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+			})();
+			</script>
+			<noscript><p><img src="{$serverurl}matomo.php?idsite={$id}&amp;rec=1&amp;action_name={$urltitle}" style="border:0;" alt="" /></p></noscript>
+		SCRIPT;
 
 		return true;
 	}
