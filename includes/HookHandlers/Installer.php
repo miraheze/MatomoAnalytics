5af95ac53c7dc67ca1606c9888b0593e8b7cdb1a
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-19 10:38:24 -0700

    Installer: use addExtensionUpdateOnVirtualDomain

--- includes/HookHandlers/Installer.php
+++ includes/HookHandlers/Installer.php
@@ -7,10 +7,21 @@ use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
 class Installer implements LoadExtensionSchemaUpdatesHook {
 
 	public function onLoadExtensionSchemaUpdates( $updater ) {
-		$updater->addExtensionTable( 'matomo',
-			__DIR__ . '/../../sql/matomo.sql' );
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-matomoanalytics',
+			'addTable',
+			'matomo',
+			__DIR__ . '/../../sql/matomo.sql',
+			true,
+		] );
 
-		$updater->addExtensionIndex( 'matomo', 'matomo_wiki',
-			__DIR__ . '/../../sql/patches/patch-matomo-add-indexes.sql' );
+		$updater->addExtensionUpdateOnVirtualDomain( [
+			'virtual-matomoanalytics',
+			'addIndex',
+			'matomo',
+			'matomo_wiki',
+			__DIR__ . '/../../sql/patches/patch-matomo-add-indexes.sql',
+			true,
+		] );
 	}
 }
