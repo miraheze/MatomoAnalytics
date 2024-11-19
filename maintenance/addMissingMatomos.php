00984be02bab28be0007d0894b44d128d7a41638
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-19 10:45:55 -0700

    Maintenance: cleanup

--- maintenance/addMissingMatomos.php
+++ maintenance/addMissingMatomos.php
@@ -2,16 +2,12 @@
 
 namespace Miraheze\MatomoAnalytics\Maintenance;
 
+$IP ??= getenv( 'MW_INSTALL_PATH' ) ?: dirname( __DIR__, 3 );
+require_once "$IP/maintenance/Maintenance.php";
+
 use Maintenance;
 use MediaWiki\MainConfigNames;
 use Miraheze\MatomoAnalytics\MatomoAnalytics;
-
-$IP = getenv( 'MW_INSTALL_PATH' );
-if ( $IP === false ) {
-	$IP = __DIR__ . '/../../..';
-}
-
-require_once "$IP/maintenance/Maintenance.php";
 
 class AddMissingMatomos extends Maintenance {
 
@@ -23,10 +19,11 @@ class AddMissingMatomos extends Maintenance {
 	}
 
 	public function execute() {
-		$dbw = $this->getDB( DB_PRIMARY, [], 'virtual-matomoanalytics' );
+		$connectionProvider = $this->getServiceContainer()->getConnectionProvider();
+		$dbr = $dbr->getReplicaDatabase( 'virtual-matomoanalytics' );
 		$databases = $this->getConfig()->get( MainConfigNames::LocalDatabases );
 		foreach ( $databases as $dbname ) {
-			$id = $dbw->selectField(
+			$id = $dbr->selectField(
 				'matomo',
 				'matomo_id',
 				[ 'matomo_wiki' => $dbname ],
