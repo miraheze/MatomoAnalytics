7d17acfbcc3947b23d768507cf36518052d7f313
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-11-19 10:50:30 -0700

    Cleanup

--- maintenance/modifyMatomo.php
+++ maintenance/modifyMatomo.php
@@ -2,18 +2,15 @@
 
 namespace Miraheze\MatomoAnalytics\Maintenance;
 
+$IP ??= getenv( 'MW_INSTALL_PATH' ) ?: dirname( __DIR__, 3 );
+require_once "$IP/maintenance/Maintenance.php";
+
 use Maintenance;
 use MediaWiki\MainConfigNames;
 use Miraheze\MatomoAnalytics\MatomoAnalytics;
 
-$IP = getenv( 'MW_INSTALL_PATH' );
-if ( $IP === false ) {
-	$IP = __DIR__ . '/../../..';
-}
-
-require_once "$IP/maintenance/Maintenance.php";
-
 class ModifyMatomo extends Maintenance {
+
 	public function __construct() {
 		parent::__construct();
 
@@ -23,14 +20,15 @@ class ModifyMatomo extends Maintenance {
 		$this->requireExtension( 'MatomoAnalytics' );
 	}
 
-	public function execute() {
+	public function execute(): void {
 		$dbname = $this->getConfig()->get( MainConfigNames::DBname );
 
 		if ( $this->getOption( 'remove', false ) ) {
 			MatomoAnalytics::deleteSite( $dbname );
-		} else {
-			MatomoAnalytics::addSite( $dbname );
+			return;
 		}
+
+		MatomoAnalytics::addSite( $dbname );
 	}
 }
 
